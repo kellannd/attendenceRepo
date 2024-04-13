e358b8c9f11f4d19a6fad13eea389f7f8fced08f
kelly deal <kellyanndeal@gmail.com>
2024-04-13 13:44:05 -0400

    create new project

---
+++ attendenceRepo/attendenceRepo/attendenceRepo.cpp
@@ -0,0 +1,21 @@
+// attendenceRepo.cpp : Defines the exported functions for the DLL.
+//
+
+#include "framework.h"
+#include "attendenceRepo.h"
+
+
+// This is an example of an exported variable
+ATTENDENCEREPO_API int nattendenceRepo=0;
+
+// This is an example of an exported function.
+ATTENDENCEREPO_API int fnattendenceRepo(void)
+{
+    return 0;
+}
+
+// This is the constructor of a class that has been exported.
+CattendenceRepo::CattendenceRepo()
+{
+    return;
+}
